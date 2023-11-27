64bde1fe79525f6fa439422234abdb4885696fff
alexsokolov <frank9154@gmail.com>
2023-11-27 22:59:15 +0300

    First commit

--- LoginPassword/LoginViewController.swift
+++ LoginPassword/LoginViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  LoginViewController.swift
 //  LoginPassword
 //
 //  Created by Александр Соколов on 27.11.2023.
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class LoginViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
